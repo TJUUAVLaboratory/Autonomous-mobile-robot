abd1ece7a60bfe60283023392edfb12f8a2bb17b
Sunshinehualong <lvhualong1995@gmail.com>
2019-06-12 15:36:57 +0800

    update

--- src/funcion_navigation/map_srv/src/map_srv.cpp
+++ src/funcion_navigation/map_srv/src/map_srv.cpp
@@ -13,7 +13,13 @@ int main(int argc, char *argv[])
     map_client.request.map_path = "/home/aibee/aibee_navi/aibee_navi_0529/exp0528/pathfind";
 
     if(client.call(map_client))
+    {
         ROS_INFO_STREAM("get the request: "<< map_client.response.message);
+        ROS_INFO_STREAM("param: " << map_client.response.parameters[0] 
+                            << " "<< map_client.response.parameters[1] 
+                            << " "<< map_client.response.parameters[2] 
+                            << " "<< map_client.response.parameters[3]);
+    }
 
     else 
         ROS_ERROR("Failed to call the service");
