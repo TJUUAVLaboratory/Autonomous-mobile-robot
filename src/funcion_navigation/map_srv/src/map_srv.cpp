8ac610c8e848c838a77e4107d28c86e9745d8dbe
Sunshinehualong <lvhualong1995@gmail.com>
2019-06-12 14:59:42 +0800

    update

--- src/funcion_navigation/map_srv/src/map_srv.cpp
+++ src/funcion_navigation/map_srv/src/map_srv.cpp
@@ -1,7 +1,7 @@
 
 #include<ros/ros.h>
 #include "map_srv/mapSave.h"
-
+//  request map save to path
 int main(int argc, char *argv[])
 {
     ros::init(argc, argv, "map_srv_node");
@@ -10,7 +10,7 @@ int main(int argc, char *argv[])
     ros::ServiceClient client = nh.serviceClient<map_srv::mapSave>("/map_save");
     map_srv::mapSave map_client;
     map_client.request.message = "save_map";
-    map_client.request.map_path = "pathfind";
+    map_client.request.map_path = "home/aibee/aibee_navi/aibee_navi_0529/exp0528/pathfind";
 
     if(client.call(map_client))
         ROS_INFO_STREAM("get the request: "<< map_client.response.message);
@@ -18,8 +18,9 @@ int main(int argc, char *argv[])
     else 
         ROS_ERROR("Failed to call the service");
 
-
     return 0;
 
 }
+
+
 
