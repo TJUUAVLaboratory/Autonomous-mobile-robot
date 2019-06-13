ef0abc3339fff61dd9237aa94860c5af9052f496
Sunshinehualong <lvhualong1995@gmail.com>
2019-06-13 12:00:49 +0800

    update

--- src/sensor_interface/pointcloud_to_laserscan/src/pointcloud_to_laserscan_nodelet.cpp
+++ src/sensor_interface/pointcloud_to_laserscan/src/pointcloud_to_laserscan_nodelet.cpp
@@ -152,7 +152,7 @@ void PointCloudToLaserScanNodelet::cloudCb(const sensor_msgs::PointCloud2ConstPt
   //ROS_INFO("subscribe_pointCloud2 messages");
   // build laserscan output
   sensor_msgs::LaserScan output;
-  static int time = 0;
+  static int time;
   if(time == 0)
   {
     output.header = cloud_msg->header;
