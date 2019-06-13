dab9c9cea87c2d05de954112a6334a763780bb8e
Sunshinehualong <lvhualong1995@gmail.com>
2019-06-13 16:42:27 +0800

    update

--- src/funcion_navigation/map_srv/src/fake_odom_test.cpp
+++ src/funcion_navigation/map_srv/src/fake_odom_test.cpp
@@ -21,7 +21,7 @@ int main(int argc, char *argv[])
     {
         for(int w=0; w<map_image.cols; ++w)
         {
-            std::cout << " " << *(map_image.at<char>(h,w));
+            ROS_INFO_STREAM(" "<< map_image.at<uchar>(h,w));
         }
         std::cout << std::endl;
     }
