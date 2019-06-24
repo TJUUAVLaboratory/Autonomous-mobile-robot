00be99077a6953547f11634d1eff9fbcf0fb4ab7
bhsun <bhsun@aibee.com>
2019-06-24 21:18:37 +0800

    add car files

---
+++ src/funcion_navigation/cmd_control/src/cmd_control_pad.cpp
@@ -0,0 +1,37 @@
+
+#include<ros/ros.h>
+#include "cmd_control/keyDataOrParam.h"
+
+
+
+
+int main(int argc, char *argv[])
+{
+    ros::init(argc, argv, "cmd_control_node");
+    ros::NodeHandle nh;
+
+    ros::Publisher pub_cmd_control = nh.advertise<cmd_control::keyDataOrParam>("keyDataOrParamU2D", 5, true);
+    cmd_control::keyDataOrParam msg_switch;
+
+    msg_switch.function = 0;  
+    msg_switch.adress = 0x02000001;  
+    msg_switch.data = 2;   //command control  not ipad
+    
+
+    ros::Rate loop_rate(50);
+    while (ros::ok())
+    {
+        pub_cmd_control.publish(msg_switch);
+        loop_rate.sleep();
+    }
+
+    msg_switch.data = 2;// ipad control
+    for(int i=0; i<51; i++)
+    {
+        pub_cmd_control.publish(msg_switch);
+    }
+
+    return 0;
+
+}
+
