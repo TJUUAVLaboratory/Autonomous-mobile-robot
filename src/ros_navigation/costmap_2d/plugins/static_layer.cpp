53a8c936c9c5e35500bb5727b50e2e0471f9df41
Sunshinehualong <lvhualong1995@gmail.com>
2019-06-27 12:07:00 +0800

    update

--- src/ros_navigation/costmap_2d/plugins/static_layer.cpp
+++ src/ros_navigation/costmap_2d/plugins/static_layer.cpp
@@ -65,7 +65,7 @@ void StaticLayer::onInitialize()
   global_frame_ = layered_costmap_->getGlobalFrameID();
 
   std::string map_topic;
-  nh.param("map_topic", map_topic, std::string("aibee_map"));
+  nh.param("map_topic", map_topic, std::string("map"));
   ROS_WARN_STREAM("the static layer map_topic:" << map_topic); //map topic
   nh.param("first_map_only", first_map_only_, false);
   nh.param("subscribe_to_updates", subscribe_to_updates_, false);
