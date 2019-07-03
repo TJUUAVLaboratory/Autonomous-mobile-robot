864d9013bd44d692018c942d090f84096072341a
Sunshinehualong <lvhualong1995@gmail.com>
2019-07-03 16:46:42 +0800

    update

--- src/ros_navigation/04_integration_navigation/global_navi/src/global_navi.cpp
+++ src/ros_navigation/04_integration_navigation/global_navi/src/global_navi.cpp
@@ -580,7 +580,7 @@ namespace global_navi {
   {
      ROS_WARN("TEST1");
      visualization_msgs::Marker waypoints_marker;
-     waypoints_marker.header.frame_id = (*waypoints)[0].header.frame_id;
+     waypoints_marker.header.frame_id = (*waypoints).at(0).header.frame_id;
      waypoints_marker.header.stamp = ros::Time::now();
      waypoints_marker.ns = global_frame_;
      waypoints_marker.action = visualization_msgs::Marker::ADD;
