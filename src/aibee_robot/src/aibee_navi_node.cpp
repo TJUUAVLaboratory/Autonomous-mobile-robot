ad4fc6eafdf88c3643522770269b0decd2b26851
Sunshinehualong <lvhualong1995@gmail.com>
2019-07-12 20:54:01 +0800

    update

--- src/aibee_robot/src/aibee_navi_node.cpp
+++ src/aibee_robot/src/aibee_navi_node.cpp
@@ -31,8 +31,8 @@
 
 int main(int argc, char** argv){
   ros::init(argc, argv, "move_base_node");
-  ros::console::set_logger_level(ROSCONSOLE_DEFAULT_NAME, ros::console::levels::Debug);
-  // ros::console::set_logger_level(ROSCONSOLE_DEFAULT_NAME, ros::console::levels::Info);
+  // ros::console::set_logger_level(ROSCONSOLE_DEFAULT_NAME, ros::console::levels::Debug);
+  ros::console::set_logger_level(ROSCONSOLE_DEFAULT_NAME, ros::console::levels::Info);
   tf::TransformListener tf(ros::Duration(10));
 
   aibee_navi::MoveBase move_base( tf );
